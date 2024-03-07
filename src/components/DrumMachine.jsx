9157500cfcbed1f2102a0046afc07426c25d4336
THalsell <139520247+THalsell@users.noreply.github.com>
2024-03-06 19:42:04 -0600

    created a drum machine

---
+++ src/components/DrumMachine.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import Pad from "./Pad";
+
+const DrumMachine = ({ myDrumPadArray }) => {
+  const [clickedId, setClickedId] = useState("");
+
+  return (
+    <div id="display">
+      {myDrumPadArray.map((item) => {
+        return <Pad srcAudio={item.srcAudio} text={item.text} id={item.id} 
+        setClickedId={setClickedId}/>;
+      })}
+      <p>{clickedId}</p>
+    </div>
+  );
+};
+
+export default DrumMachine;
